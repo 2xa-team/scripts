865140ff5ba1ae03e52e0c54adc9e5c290990a9d
Junk <45537049+JunkCoded@users.noreply.github.com>
2025-07-05 13:43:17 +0700

    Update vpn-tgbot-deploy-prod.sh

--- vpn-tgbot-deploy-prod.sh
+++ vpn-tgbot-deploy-prod.sh
@@ -4,7 +4,8 @@ set -e
 REPO_URL="git@github.com:2xa-team/vpn-tgbot.git"
 REPO_DIR="vpn-tgbot"
 BRANCH="main"
-FILES="docker-compose.prod.yaml example.env"
+FILES="docker-compose.prod.yaml
+example.env"
 
 mkdir "$REPO_DIR"
 cd "$REPO_DIR"
@@ -12,10 +13,10 @@ cd "$REPO_DIR"
 git init
 git remote add origin "$REPO_URL"
 git config core.sparseCheckout true
-git config core.sparseCheckoutCone true
+
+echo "$FILES" > .git/info/sparse-checkout
+
 git fetch origin "$BRANCH"
-git sparse-checkout init --cone
-git sparse-checkout set $FILES
 git checkout "$BRANCH"
 
 echo "✅ Files $FILES uploaded in $(pwd)"
